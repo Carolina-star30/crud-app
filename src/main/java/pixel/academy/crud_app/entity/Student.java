754a9150e62779f5b5e76f7f320f14cd01c39ce5
Carolina Varzari <varzari3009@gmail.com>
2026-03-14 16:56:22 +0200

    Generate constructori

--- src/main/java/pixel/academy/crud_app/entity/Student.java
+++ src/main/java/pixel/academy/crud_app/entity/Student.java
@@ -22,4 +22,57 @@ public class Student {
     @Column(name="email")
     private String email;
 
+    // definirea constructurilor
+    public Student() {
+
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // definirea get si set
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
