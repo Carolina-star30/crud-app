1fc0648e3fc4755d39b16300e84bac0ebee51b10
Carolina Varzari <varzari3009@gmail.com>
2026-03-17 19:09:14 +0200

    Add a student in DB

--- src/main/java/pixel/academy/crud_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import pixel.academy.crud_app.dao.StudentDAO;
+import pixel.academy.crud_app.entity.Student;
 
 @SpringBootApplication
 public class CrudAppApplication {
@@ -16,10 +18,23 @@ public class CrudAppApplication {
 
 
 @Bean
-public CommandLineRunner commandLineRunner(String[] args) {
+public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 	return runner -> {
-		System.out.println("Hello, world!");
+		createStudent(studentDAO);
 	};
+	}
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create a Student object
+		System.out.println("Creating a new student object ...");
+		Student newStudent = new Student("John", "Doe", "john@pixelacademy.md");
+
+		//save the object in BD with DAY
+		System.out.println("Saving the student ...");
+		studentDAO.save(newStudent);
+
+		// GET ID-ul the save student
+		System.out.printf("Saved student. Generated id: " + newStudent.getId() );
 	}
 }
